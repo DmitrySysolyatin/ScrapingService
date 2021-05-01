8b81658bc76e91275e563f818d661b319d38895c
DmitrySysolyatin <mitya-sysolyatin@mail.ru>
2021-05-01 20:18:06 +0300

    Commit 4

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -19,7 +19,24 @@ class Language(models.Model):
 
     class Meta:
         # verbose_name = 'Город'
-        verbose_name_plural = 'Languages'
+        verbose_name_plural = 'Programming languages'
 
     def __str__(self):
         return self.name
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name ='Title')
+    company = models.CharField(max_length=250, verbose_name='Company')
+    description = models.TextField(verbose_name='Vacation description')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='City')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 verbose_name='Programming language')
+    timestamp = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Vacancies'
+
+    def __str__(self):
+        return self.title
